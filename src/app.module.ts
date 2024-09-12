3d6ee57961f45932642c6735e3554c3203433bc4
Ivan Petrukhin <monkpatch@gmail.com>
2024-09-12 17:31:48 +0300

    Make configuration in env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PingModule } from './ping/ping.module';
 
 @Module({
-  imports: [PingModule],
+  imports: [PingModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  static port: number = 3000;
+  static hostname: string = '0.0.0.0';
+
+  constructor(configService: ConfigService) {
+    const addr = configService.get<string>('SERVER_ADDRESS', '');
+    const [hostname, strPort] = addr.split(':');
+    const port = parseInt(strPort);
+    if (!hostname || !strPort || Number.isNaN(AppModule.port))
+      throw new Error('Invalid SERVER_ADDRESS');
+
+    AppModule.hostname ||= hostname;
+    AppModule.port ||= port;
+  }
+}
