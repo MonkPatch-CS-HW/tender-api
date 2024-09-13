2708f87743c08dd95d83c9404e5a9cc00ef9fb3a
Ivan Petrukhin <monkpatch@gmail.com>
2024-09-13 17:40:15 +0300

    Format errors like { reason: string }

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './misc/AllExceptionsFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const httpAdapter = app.get(HttpAdapterHost);
+
   app.setGlobalPrefix('api');
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -15,4 +19,5 @@ async function bootstrap() {
   app.enableCors();
   await app.listen(8080);
 }
+
 bootstrap();
