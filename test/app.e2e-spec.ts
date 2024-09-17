ee2a6e80d5145866a58df893809715322959caef
Ivan Petrukhin <monkpatch@gmail.com>
2024-09-17 15:48:50 +0300

    Regroup tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -147,145 +147,198 @@ describe('AppController (e2e)', () => {
       await initTenders(prisma, config);
     });
 
-    it('/tenders/new - incorrect request', () => {
-      return request(app.getHttpServer()).post('/tenders/new').expect(400);
+    describe('/new', () => {
+      it('incorrect request', () => {
+        return request(app.getHttpServer()).post('/tenders/new').expect(400);
+      });
+
+      it('invalid user', () => {
+        return request(app.getHttpServer())
+          .post('/tenders/new')
+          .send({
+            name: 'string',
+            description: 'string',
+            serviceType: 'Construction',
+            organizationId: config.avitoOrgId,
+            creatorUsername: 'INVALID',
+          })
+          .expect(401);
+      });
+
+      it('invalid organization', () => {
+        return request(app.getHttpServer())
+          .post('/tenders/new')
+          .send({
+            name: 'string',
+            description: 'string',
+            serviceType: 'Construction',
+            organizationId: 'INVALID',
+            creatorUsername: config.avitoOrgId,
+          })
+          .expect(401);
+      });
+
+      it('insiffucient rights', () => {
+        return request(app.getHttpServer())
+          .post('/tenders/new')
+          .send({
+            name: 'string',
+            description: 'string',
+            serviceType: 'Construction',
+            organizationId: config.avitoOrgId,
+            creatorUsername: config.yandexEmpUser,
+          })
+          .expect(403);
+      });
+
+      it('actually creates', () => {
+        return request(app.getHttpServer())
+          .post('/tenders/new')
+          .send({
+            name: 'string',
+            description: 'string',
+            serviceType: 'Construction',
+            organizationId: config.avitoOrgId,
+            creatorUsername: config.avitoEmpUser,
+          })
+          .expect(201)
+          .expect(async (response: Response & { body: TenderData }) => {
+            expect(response.body).toHaveProperty('id');
+            const id = response.body.id;
+            const tender = await prisma.tender.findFirst({ where: { id } });
+            expect(tender).not.toBeNull();
+          });
+      });
     });
 
-    it('/tenders/new - invalid user', () => {
-      return request(app.getHttpServer())
-        .post('/tenders/new')
-        .send({
-          name: 'string',
-          description: 'string',
-          serviceType: 'Construction',
-          organizationId: config.avitoOrgId,
-          creatorUsername: 'INVALID',
-        })
-        .expect(401);
+    describe('/', () => {
+      it('invalid query', () => {
+        return request(app.getHttpServer())
+          .get('/tenders?usernama=INVALID&limit=-1')
+          .expect(400);
+      });
+
+      it('shows and only shows open', () => {
+        return request(app.getHttpServer())
+          .get('/tenders')
+          .expect(200)
+          .expect((response: Response & { body: TenderData[] }) => {
+            return (
+              response.body.length === 1 &&
+              response.body[0].organizationId === config.avitoOrgId &&
+              response.body[0].serviceType === tenderServiceType.Delivery
+            );
+          });
+      });
+
+      it('filter by serviceType incorrect ', () => {
+        return request(app.getHttpServer())
+          .get(`/tenders?service_type=INVALID`)
+          .expect(400);
+      });
+
+      it('filter by serviceType includes matching', () => {
+        return request(app.getHttpServer())
+          .get(`/tenders?service_type=${tenderServiceType.Delivery}`)
+          .expect(200)
+          .expect((response: Response & { body: TenderData[] }) => {
+            expect(response.body).toHaveLength(1);
+            expect(response.body[0].organizationId).toBe(config.avitoOrgId);
+            expect(response.body[0].serviceType).toBe(
+              tenderServiceType.Delivery,
+            );
+          });
+      });
+
+      it('filter by serviceType does not include not matching', () => {
+        return request(app.getHttpServer())
+          .get(
+            `/tenders?service_type=${tenderServiceType.Construction}&service_type=${tenderServiceType.Manufacture}`,
+          )
+          .expect(200)
+          .expect((response: Response & { body: TenderData[] }) => {
+            expect(response.body).toHaveLength(0);
+          });
+      });
+
+      it('limit works', () => {
+        return request(app.getHttpServer())
+          .get('/tenders?limit=0')
+          .expect(200)
+          .expect((response: Response & { body: TenderData[] }) => {
+            expect(response.body).toHaveLength(0);
+          });
+      });
+
+      it('offset works', () => {
+        return request(app.getHttpServer())
+          .get('/tenders?offset=1')
+          .expect(200)
+          .expect((response: Response & { body: TenderData[] }) => {
+            expect(response.body).toHaveLength(0);
+          });
+      });
     });
 
-    it('/tenders/new - invalid organization', () => {
-      return request(app.getHttpServer())
-        .post('/tenders/new')
-        .send({
-          name: 'string',
-          description: 'string',
-          serviceType: 'Construction',
-          organizationId: 'INVALID',
-          creatorUsername: config.avitoOrgId,
-        })
-        .expect(401);
-    });
+    describe('/my', () => {
+      it('invalid username', () => {
+        return request(app.getHttpServer())
+          .get('/tenders/my?username=INVALID')
+          .expect(401);
+      });
 
-    it('/tenders/new - insiffucient rights', () => {
-      return request(app.getHttpServer())
-        .post('/tenders/new')
-        .send({
-          name: 'string',
-          description: 'string',
-          serviceType: 'Construction',
-          organizationId: config.avitoOrgId,
-          creatorUsername: config.yandexEmpUser,
-        })
-        .expect(403);
-    });
+      it('invalid query', () => {
+        return request(app.getHttpServer())
+          .get('/tenders/my?usernama=INVALID&limit=-1')
+          .expect(400);
+      });
 
-    it('/tenders/new - valid', () => {
-      return request(app.getHttpServer())
-        .post('/tenders/new')
-        .send({
-          name: 'string',
-          description: 'string',
-          serviceType: 'Construction',
-          organizationId: config.avitoOrgId,
-          creatorUsername: config.avitoEmpUser,
-        })
-        .expect(201);
-    });
+      it('shows open', () => {
+        return request(app.getHttpServer())
+          .get(`/tenders/my?username=${config.avitoEmpUser}`)
+          .expect(200)
+          .expect((response: Response & { body: TenderData[] }) => {
+            expect(response.body).toHaveLength(1);
+            expect(response.body[0].organizationId).toBe(config.avitoOrgId);
+          });
+      });
 
-    it('/tenders - shows and only shows open', () => {
-      return request(app.getHttpServer())
-        .get('/tenders')
-        .expect(200)
-        .expect((response: Response & { body: TenderData[] }) => {
-          return (
-            response.body.length === 1 &&
-            response.body[0].organizationId === config.avitoOrgId &&
-            response.body[0].serviceType === tenderServiceType.Delivery
-          );
-        });
-    });
+      it('shows closed', () => {
+        return request(app.getHttpServer())
+          .get(`/tenders/my?username=${config.yandexEmpUser}`)
+          .expect(200)
+          .expect((response: Response & { body: TenderData[] }) => {
+            expect(response.body).toHaveLength(1);
+            expect(response.body[0].organizationId).toBe(config.yandexOrgId);
+          });
+      });
 
-    it('/tenders - filter by serviceType incorrect ', () => {
-      return request(app.getHttpServer())
-        .get(`/tenders?service_type=INVALID`)
-        .expect(400);
-    });
+      it('limit works', () => {
+        return request(app.getHttpServer())
+          .get(`/tenders/my?username=${config.yandexEmpUser}&limit=0`)
+          .expect(200)
+          .expect((response: Response & { body: TenderData[] }) => {
+            expect(response.body).toHaveLength(0);
+          });
+      });
 
-    it('/tenders - filter by serviceType includes matching', () => {
-      return request(app.getHttpServer())
-        .get(`/tenders?service_type=${tenderServiceType.Delivery}`)
-        .expect(200)
-        .expect((response: Response & { body: TenderData[] }) => {
-          expect(response.body).toHaveLength(1);
-          expect(response.body[0].organizationId).toBe(config.avitoOrgId);
-          expect(response.body[0].serviceType).toBe(tenderServiceType.Delivery);
-        });
-    });
+      it('offset works', () => {
+        return request(app.getHttpServer())
+          .get(`/tenders/my?username=${config.yandexEmpUser}&offset=1`)
+          .expect(200)
+          .expect((response: Response & { body: TenderData[] }) => {
+            expect(response.body).toHaveLength(0);
+          });
+      });
 
-    it('/tenders - filter by serviceType does not include not matching', () => {
-      return request(app.getHttpServer())
-        .get(
-          `/tenders?service_type=${tenderServiceType.Construction}&service_type=${tenderServiceType.Manufacture}`,
-        )
-        .expect(200)
-        .expect((response: Response & { body: TenderData[] }) => {
-          expect(response.body).toHaveLength(0);
-        });
-    });
-
-    it('/tenders - limit works', () => {
-      return request(app.getHttpServer())
-        .get('/tenders?limit=0')
-        .expect(200)
-        .expect((response: Response & { body: TenderData[] }) => {
-          expect(response.body).toHaveLength(0);
-        });
-    });
-
-    it('/tenders - offset works', () => {
-      return request(app.getHttpServer())
-        .get('/tenders?offset=1')
-        .expect(200)
-        .expect((response: Response & { body: TenderData[] }) => {
-          expect(response.body).toHaveLength(0);
-        });
-    });
-
-    it('/tenders/my - invalid username', () => {
-      return request(app.getHttpServer())
-        .get('/tenders/my?username=INVALID')
-        .expect(401);
-    });
-
-    it('/tenders/my - shows open', () => {
-      return request(app.getHttpServer())
-        .get(`/tenders/my?username=${config.avitoEmpUser}`)
-        .expect(200)
-        .expect((response: Response & { body: TenderData[] }) => {
-          expect(response.body).toHaveLength(1);
-          expect(response.body[0].organizationId).toBe(config.avitoOrgId);
-        });
-    });
-
-    it('/tenders/my - shows closed', () => {
-      return request(app.getHttpServer())
-        .get(`/tenders/my?username=${config.yandexEmpUser}`)
-        .expect(200)
-        .expect((response: Response & { body: TenderData[] }) => {
-          expect(response.body).toHaveLength(1);
-          expect(response.body[0].organizationId).toBe(config.yandexOrgId);
-        });
+      it('offset works', () => {
+        return request(app.getHttpServer())
+          .get(`/tenders/my?username=${config.yandexEmpUser}&offset=1`)
+          .expect(200)
+          .expect((response: Response & { body: TenderData[] }) => {
+            expect(response.body).toHaveLength(0);
+          });
+      });
     });
   });
 });
